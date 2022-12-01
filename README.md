5bae90ea85073337e12527f5523a8d56f2f0ba6d
LeunamSenuf <leunamsenuf@gmail.com>
2022-12-01 18:19:32 -0300

    Init Upload

--- README.md
+++ README.md
@@ -3,3 +3,28 @@ BY manuelfunes@yahoo.com.br
 VERSION 0.0
 
 
+Teste – Capgemini
+Você foi contratado para desenvolver um projeto em Go, Java, Python ou Javascript (NodeJS),
+que vai identificar se uma sequência de letras é válida.
+O projeto consiste em desenvolver uma API REST, e disponibilizar um endpoint HTTP POST
+"/sequence". Esse endpoint receberá como parâmetro, um JSON com a sequência de letras
+(Array de Strings), onde, cada elemento desse array representa uma linha de uma tabela
+quadrada de (NxN), como no exemplo abaixo:
+
+```
+POST -> /sequence
+{
+"letters": ["DUHBHB", "DUBUHD", "UBUUHU", "BHBDHH", " DDDDUB", "UDBDUH"]
+}
+
+Você saberá se uma sequência é válida, se encontrar 2 ou mais sequências de quatro
+letras iguais em qualquer direção, horizontal, vertical ou nas diagonais.
+As letras da String só podem ser: (B, U, D, H)
+A API deve retornar um json com "is_valid": boolean. Caso você identifique uma sequência
+válida, deve ser true, caso identifique uma sequência inválida, deve ser false, como no
+exemplo abaixo:
+HTTP 200
+{"is_valid": true}
+
+
+'''
--- main.go
+++ main.go
@@ -1,17 +1,68 @@
+/*
+* Sample API with GET and POST endpoint.
+* POST data is converted to string and saved in internal memory.
+* GET endpoint returns all strings in an array.
+ */
 package main
 
 import (
-	"challengeDNA/router"
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
-func main() {
-	r := router.Router()
-	// fs := http.FileServer(http.Dir("build"))
-	// http.Handle("/", fs)
-	fmt.Println("Starting server on the port 8080...")
+var (
+	// flagPort is the open port the application listens on
+	flagPort = flag.String("port", "9000", "Port to listen on")
+)
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+var results []string
+
+// GetHandler handles the index route
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	jsonBody, err := json.Marshal(results)
+	if err != nil {
+		http.Error(w, "Error converting results to json",
+			http.StatusInternalServerError)
+	}
+
+	fmt.Println(results[1])
+	w.Write(jsonBody)
+
+}
+
+// PostHandler converts post request body to string
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body",
+				http.StatusInternalServerError)
+		}
+		results = append(results, string(body))
+
+		fmt.Fprint(w, "POST done")
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+}
+
+func init() {
+	log.SetFlags(log.Lmicroseconds | log.Lshortfile)
+	flag.Parse()
+}
+
+func main() {
+	results = append(results, time.Now().Format(time.RFC3339))
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", GetHandler)
+	mux.HandleFunc("/post", PostHandler)
+
+	log.Printf("listening on port %s", *flagPort)
+	log.Fatal(http.ListenAndServe(":"+*flagPort, mux))
 }
